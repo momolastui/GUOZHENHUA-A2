aef8283b7d9f41f0ce1a0118b3847c70d64ecf77
monoLastui <2549844465@qq.com>
2024-12-03 16:47:11 +0800

    2

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,46 @@
-public class Person {
-    
+//基类，内容由Employee 和 Visitor 继承
+public abstract class Person {
+    private String name;
+    private int age;
+    private String address;
+
+    // Default constructor
+    public Person() {
+        this.name = "Unknown";
+        this.age = 0;
+        this.address = "Unknown";
+    }
+
+    // Parameterized constructor
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
+
