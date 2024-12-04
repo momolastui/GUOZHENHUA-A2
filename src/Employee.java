1c810057689aaffd87f1a3b912d205cb59964d68
monoLastui <2549844465@qq.com>
2024-12-04 10:51:45 +0800

    4

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,36 @@
-public class Employee {
-    
+public class Employee extends Person {
+    private String jobTitle;
+    private int employeeID;
+
+    // 默认构造函数
+    public Employee() {
+        super(); // 调用 Person 的构造函数
+        this.jobTitle = "未知";
+        this.employeeID = 0;
+    }
+
+    // 参数化构造函数
+    public Employee(String name, int age, String address, String jobTitle, int employeeID) {
+        super(name, age, address); // 调用 Person 的构造函数
+        this.jobTitle = jobTitle;
+        this.employeeID = employeeID;
+    }
+
+    // Getter 和 Setter 方法
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
 }
+
--- src/RideInterface.java
+++ src/RideInterface.java
@@ -1,3 +1,11 @@
-public class RideInterface {
-    
+// RideInterface 接口定义
+public interface RideInterface {
+    void addVisitorToQueue(Visitor visitor);
+    void removeVisitorFromQueue(Visitor visitor);
+    void printQueue();
+    void runOneCycle();
+    void addVisitorToHistory(Visitor visitor);
+    void checkVisitorFromHistory(Visitor visitor);
+    int numberOfVisitors();
+    void printRideHistory();
 }
